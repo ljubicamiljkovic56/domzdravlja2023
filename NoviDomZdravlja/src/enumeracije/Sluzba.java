c7294c599457dcf0ec72c43fd80dce5f5243f8be
ljubicamiljkovic56 <ljubica.m19@gmail.com>
2023-01-18 20:47:45 +0100

    Finished with enumeration and object classes

--- NoviDomZdravlja/src/enumeracije/Sluzba.java
+++ NoviDomZdravlja/src/enumeracije/Sluzba.java
@@ -1,5 +1,44 @@
 package enumeracije;
 
 public enum Sluzba {
+	Sluzba_opste_medicine,
+	Sluzba_zdravstvene_zastite_dece,
+	Stomatološka_sluzba,
+	Sluzba_zdravstvene_zastite_radnika,
+	Sluzba_za_pravne_i_ekonomske_poslove,
+	Sluzba_za_tehnicke_poslove; 
+	
+	public static Sluzba valueOf(int a) {
+		switch(a) {
+		case 1:
+			return Sluzba_opste_medicine;
+		case 2:
+			return Sluzba_zdravstvene_zastite_dece;
+		case 3:
+			return Stomatološka_sluzba;
+		case 4:
+			return Sluzba_zdravstvene_zastite_radnika;
+		case 5:
+			return Sluzba_za_pravne_i_ekonomske_poslove;
+		default:
+			return Sluzba_za_tehnicke_poslove;
+		}
+	}
+	public static int toInt(Sluzba sluzba) {
+		switch(sluzba) {
+		case Sluzba_opste_medicine:
+			return 1;
+		case Sluzba_zdravstvene_zastite_dece:
+			return 2;
+		case Stomatološka_sluzba:
+			return 3;
+		case Sluzba_zdravstvene_zastite_radnika:
+			return 4;
+		case Sluzba_za_pravne_i_ekonomske_poslove:
+			return 5;
+		default:
+			return 6;
+		}
+	}
 
 }
--- NoviDomZdravlja/src/enumeracije/Status.java
+++ NoviDomZdravlja/src/enumeracije/Status.java
@@ -1,5 +1,34 @@
 package enumeracije;
 
 public enum Status {
-
+	zatrazen,
+	zakazan,
+	otkazan,
+	zavrsen;
+	
+	public static Status valueOf(int a) {
+		switch (a) {
+		case 1:
+			return zatrazen;
+		case 2:
+			return zakazan;
+		case 3:
+			return otkazan;
+		default:
+			return zavrsen;
+		}
+	}
+	
+	public static int toInt(Status status) {
+		switch (status) {
+		case zatrazen:
+			return 1;
+		case zakazan:
+			return 2;
+		case otkazan:
+			return 3;
+		default:
+			return 4;
+		}
+	}
 }
